c7c837ba664b61f62618047fd64c47161b2bd521
Job Daniel Correa <jobcorre0912@gmail.com>
2026-04-14 18:16:09 -0500

    Actualizar mair - Agremamos para hacer pruebas de producto

--- Main.java
+++ Main.java
@@ -1,3 +1,12 @@
 public class Main{
-    
+
+    public static void main(String[] args) {
+        Producto p1 = new Producto("Shampoo", 15000, 45);
+        
+        // Acceder a los miembros (funciona porque estamos en el mismo paquete)
+        System.out.println("Accediendo directamente al nombre: " + p1.nombre);
+        System.out.println("Accediendo directamente al precio: " + p1.precio);
+        System.out.println("Accediendo directamente al stock: " + p1.stock);
+        p1.mostrarInfo();
+    }
 }
